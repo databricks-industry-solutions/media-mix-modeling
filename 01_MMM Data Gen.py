2e3991115f5b38883ea50249b8ca526b94b69cce
corey.abshire@databricks.com <corey.abshire@databricks.com>
2023-05-16 16:32:10 +0000

    Add text. Fix libs (again)
    Added some text to get us started on this one as per discussion with Layla. We can extend it from here.
    
    Also, the libs were broken again somehow, so I ended up needing to move it back to %pip install in the config notebook (except pytest). Hopefully this will be stable.

--- 01_MMM Data Gen.py
+++ 01_MMM Data Gen.py
@@ -5,15 +5,15 @@
 
 # MAGIC %md 
 # MAGIC ## Media Mix Modeling Accelerator
-# MAGIC 
+# MAGIC
 # MAGIC MMM (Marketing or Media Mix Modeling), is a data-driven methodology that enables companies to identify and measure the impact of their marketing campaigns across multiple channels.  MMM helps businesses make better-informed decisions about their advertising and marketing strategies. 
-# MAGIC 
+# MAGIC
 # MAGIC By analyzing data from various channels such as TV, social media, email marketing, and more, MMM can determine which channels are contributing the most to strategic KPIs, such as sales. By including external events and indicators, decision makers can better understand the impact of outside factors (such as holidays, economic conditions, or weather) and avoid accidently over-valuing the impact of ad spend alone.
-# MAGIC 
-# MAGIC Databricks Lakehouse offers a unified platform for building modernized MMM solutions that are scalable and flexible. Marketers can unifies various upstream data sources, automates data ingestion, processing, and transformation, and provides full transparency and traceability of data usage. With powerful DSML capabilities and collaborative workstream, marketers can seamlessly leverage the full potential of their data, driving more informed and effective marketing investment decisions. 
-# MAGIC 
-# MAGIC In this notebook, we set up the environment to generate some synthetic data we will use in the second notebook, where we showcase how to build a media mix model using the classes we wrote based on pymc3.
-# MAGIC 
+# MAGIC
+# MAGIC Databricks Lakehouse offers a unified platform for building modernized MMM solutions that are scalable and flexible. Marketers can unify various upstream data sources, automates data ingestion, processing, and transformation, and provides full transparency and traceability of data usage. With powerful data science and machine learning capabilities and collaborative workstreams, marketers can seamlessly leverage the full potential of their data, driving more informed and effective marketing investment decisions. 
+# MAGIC
+# MAGIC In this notebook, we set up the environment to generate some synthetic data we will use in the second notebook, where we showcase how to build a media mix model using the classes we wrote based on [pymc3](https://www.pymc.io/projects/docs/en/v3/index.html).
+# MAGIC
 # MAGIC <insert architecture diagram>
 
 # COMMAND ----------
@@ -23,6 +23,8 @@
 # COMMAND ----------
 
 # MAGIC %md ### Step 1: Set up the environment
+# MAGIC
+# MAGIC First, we need to import a couple of libraries we use to generate the dataset. Also, we've moved the data generation logic into a separate module so we import that as well. This shows how Databricks Repos and Workspace files works with Notebooks to allow you to easily refactor code into reusable modules as you develop your pipelines and models. We also set a seed to help with reproducibility.
 
 # COMMAND ----------
 
@@ -41,6 +43,12 @@ np.random.seed(RANDOM_SEED)
 # COMMAND ----------
 
 # MAGIC %md ### Step 2: Define the schema
+# MAGIC
+# MAGIC Next, we define a schema for the table we're about to generate. The idea with this accelerator is to simulate a gold table where all the daily aggregate marketing spend has already been cleansed and published into the Lakehouse for our marketing team to use. In your application, a similar pipeline may have already been developed by upstream teams for you, or you can use the Databricks Lakehouse to build your pipeline all the way from ingesting from the raw sources! 
+# MAGIC
+# MAGIC For your analysis, you may have many more spend channels to analyze, other dependent variables and KPI's to analyze them against, or be working at a higher or lower time series granularity (weekly is common in addition to daily). Also, you may have indicator variables to consider to represent the presence of holidays or other events, or include grouping factors by which you can slice your analysis into multiple models or to include in a larger model, such as brands, business units, or geographical regions. 
+# MAGIC
+# MAGIC For this example, we'll keep it simple and just generate three marketing spend columns for a couple of example channels, along with a date column and a dependent variable, sales. 
 
 # COMMAND ----------
 
@@ -55,6 +63,8 @@ schema = StructType([
 # COMMAND ----------
 
 # MAGIC %md ### Step 3: Generate the dataset
+# MAGIC
+# MAGIC After this we can generate the dataset. The generator code is fairly straightforward, but a little long to include inline in the notebook. It has been refactored into a `Generator` class which contains multiple `Channel` instances, each of which support either a decay effect (geometric adstock), a saturation effect (logistic), or both of those or neither. It also supports a weighting factor called \\(\beta\\) the controls the impact on sales. The specific parameter values are stored in a `yaml` configuration file in the config directory, so feel free to copy that file and experiment with it to more closely mimic your specific business case. You can also explore and extend the `Generator` itself to create your own simulator based on whatever data you'd like to try to recover with your downstream model. We'll say more about the model itself as we get into the modeling notebook later on.
 
 # COMMAND ----------
 
@@ -66,9 +76,22 @@ df.plot(linewidth=0.25);
 # COMMAND ----------
 
 # MAGIC %md ### Step 4: Write the simulated gold table
+# MAGIC
+# MAGIC We generated the dataset above as a *pandas* `DataFrame`, which is fine for a small dataset like this, but in many cases your data will be coming from a gold 🥇 table that is fed by upstream data sources and stored in your Databricks Lakehouse, ideally in [Unity Catalog](https://www.databricks.com/product/unity-catalog). Here, we'll simulate that aspect as well by converting the *pandas* `DataFrame` to a *spark* `DataFrame` and writing it out to a catalog created by our setup script as a [Delta](https://docs.databricks.com/delta/index.html) table.
+
+# COMMAND ----------
+
+# MAGIC %pip install pandas==1.3.4
 
 # COMMAND ----------
 
 sdf = mmg.convert_to_spark_dataframe(df, schema)
 sdf.write.mode('overwrite').saveAsTable(gold_table_name)
 display(sdf)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Data generation ✅
+# MAGIC
+# MAGIC That's all for the data generation notebook. Next, head on over to the "02_MMM PyMC Example" notebook to see if we can recover those parameters with our Bayesian modeling exercise!
